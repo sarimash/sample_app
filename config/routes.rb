ba077847c795ad915f4c0a64ff97983b4c3f4151
Brandon Stickle <brandon.stickle@gmail.com>
2014-03-23 15:40:21 -0700

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ SampleApp::Application.routes.draw do
   match '/signup', 	to: 'users#new',	 	via: 'get'
   match '/signin', 	to: 'sessions#new', 		via: 'get'
   match '/signout', 	to: 'sessions#destroy', 	via: 'delete'
+  
 
   match '/help', 	to: 'static_pages#help', 	via: 'get'
   match '/about', 	to: 'static_pages#about', 	via: 'get'
